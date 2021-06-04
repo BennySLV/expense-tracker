e64193dc7159980c4d1584175d997c794e6a1b38
Ben Silveston <bsilveston37@gmail.com>
2021-06-04 22:28:55 +0100

    Updated the ExpenseItem component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,13 @@
 function ExpenseItem() {
-	return <h2>Expense item</h2>;
+	return (
+		<div>
+			<div>4th June 2021</div>
+			<div>
+				<h2>Car Insurance</h2>
+				<div>£294.67</div>
+			</div>
+		</div>
+	);
 }
 
 export default ExpenseItem;
